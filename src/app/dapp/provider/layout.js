c2c89c98344aa3947e2c9403886f46408800d50c
hfccr <hfccr@outlook.com>
2023-10-02 04:35:06 +0530

    Fix name

--- src/app/dapp/provider/layout.js
+++ src/app/dapp/provider/layout.js
@@ -4,7 +4,7 @@ import { Tab, Box, Divider, Stack, Tabs, Typography } from "@mui/material";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function ClientLayout({ children }) {
+export default function ProviderLayout({ children }) {
   const path = usePathname();
   let selected = "about";
   if (path.indexOf("deals") >= 0) {
